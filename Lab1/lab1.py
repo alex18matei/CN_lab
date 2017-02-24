e0976ef57418b05d99cfde99bf8cccc56b1ca34c
Cezar <cezar.elnazli2@gmail.com>
2017-02-24 15:36:17 +0200

    Refactorizare pentru exercițiul 3

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -43,42 +43,45 @@ c5 = 1 / math.factorial(11)
 c6 = 1 / math.factorial(13)
 
 
-def calculate_polynom(n, x):
+def compute_polynomial(n, x):
     y = x ** 2
     p = 1
-    if n is 1:
+    if n == 1:
         p = x * (1 + y * (-c1 + y * c2))
-    elif n is 2:
+    elif n == 2:
         p = x * (1 + y * (-c1 + y * (c2 - c3 * y)))
-    elif n is 3:
+    elif n == 3:
         p = x * (1 + y * (-c1 + y * (c2 + y * (-c3 + y * c4))))
-    elif n is 4:
+    elif n == 4:
         p = x * (1 + y * (-0.1666 + y * (0.00833 + y * (-c3 + y * c4))))
-    elif n is 5:
+    elif n == 5:
         p = x * (1 + y * (-c1 + y * (c2 + y * (-c3 + y * (c4 - c5 * y)))))
-    elif n is 6:
+    elif n == 6:
         p = x * (1 + y * (-c1 + y * (c2 + y * (-c3 + y * (c4 + y * (-c5 + y * c6))))))
     return p
 
 
 def ex3():
-    number_list = [random.uniform(-math.pi / 2, math.pi / 2) for x in range(0, 10000)]
+    number_list = [random.uniform(-math.pi / 2, math.pi / 2)
+                   for x in range(10000)]
     count = {i: 0 for i in range(1, 7)}
 
     for number in number_list:
-        polynom_results = [calculate_polynom(polynom_index, number) for polynom_index in range(1, 7)]
-        polynom_errors = [abs(math.sin(number) - res) for res in polynom_results]
+        results = [compute_polynomial(i, number) for i in range(1, 7)]
+        errors = [abs(res - math.sin(number)) for res in results]
 
-        best_polynom = polynom_errors.index(min(polynom_errors)) + 1
-        count[best_polynom] += 1
+        best_polynomial = errors.index(min(errors)) + 1
+        count[best_polynomial] += 1
     print(count)
 
-    number_list = [random.uniform(-math.pi / 2, math.pi / 2) for x in range(0, 100000)]
-    for polynom in range(1, 7):
+    number_list = [random.uniform(-math.pi / 2, math.pi / 2)
+                   for x in range(100000)]
+    for polynomial in range(1, 7):
         start_time = time.time()
         for number in number_list:
-            calculate_polynom(polynom, number)
-        print("Polynom " + str(polynom) + " : " + str(time.time() - start_time))
+            compute_polynomial(polynomial, number)
+        print("polynomial {}: {}"
+              .format(str(polynomial), str(time.time() - start_time)))
 
 
 if __name__ == '__main__':
