868220dd4ffe6d17d0ecd55737e15beaf963dbf7
Cezar <cezar.elnazli2@gmail.com>
2017-02-24 18:06:11 +0200

    Afișare mai frumoasă

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -4,7 +4,18 @@ import random
 import time
 
 
+c1 = 1 / math.factorial(3)
+c2 = 1 / math.factorial(5)
+c3 = 1 / math.factorial(7)
+c4 = 1 / math.factorial(9)
+c5 = 1 / math.factorial(11)
+c6 = 1 / math.factorial(13)
+
+
 def ex1():
+    print('Exercitiul 1')
+    print('============')
+
     u = 10
     d = 1.0
     m = 0
@@ -13,16 +24,24 @@ def ex1():
         u = math.pow(10, -m)
         m = m + 1
 
-    print(u)
+    print('u = {}'.format(u))
+    print('')
+
     return u
 
 
 def ex2(u):
+    print('Exercitiul 2')
+    print('============')
+
     x = 1.0
     y = u
     z = u
     if (x + y) + z != x + (y + z):
-        print("adunare neasociativa")
+        print(">>> ({x} + {y}) + {z} != {x} + ({y} + {z})"
+              .format(x=x, y=y, z=z))
+        print((x + y) + z != x + (y + z))
+        print('')
 
     x = math.pow(10, -random.randint(1, 16))
     y = math.pow(10, -random.randint(1, 16))
@@ -32,16 +51,11 @@ def ex2(u):
         x = math.pow(10, -random.randint(1, 16))
         y = math.pow(10, -random.randint(1, 16))
         z = math.pow(10, -random.randint(1, 16))
-    else:
-        print("inmultire neasociativa ", x, y, z)
 
-
-c1 = 1 / math.factorial(3)
-c2 = 1 / math.factorial(5)
-c3 = 1 / math.factorial(7)
-c4 = 1 / math.factorial(9)
-c5 = 1 / math.factorial(11)
-c6 = 1 / math.factorial(13)
+    print(">>> ({x} * {y}) * {z} == {x} * ({y} * {z})"
+          .format(x=x, y=y, z=z))
+    print((x * y) * z == x * (y * z))
+    print('')
 
 
 def compute_polynomial(n, x):
@@ -63,34 +77,33 @@ def compute_polynomial(n, x):
 
 
 def ex3():
-    number_list = [random.uniform(-math.pi / 2, math.pi / 2)
-                   for x in range(10000)]
-    count = {i: 0 for i in range(1, 7)}
+    print('Exercitiul 3')
+    print('============')
+
+    nums = [random.uniform(-math.pi / 2, math.pi / 2) for x in range(10000)]
 
     best = {}
     best_each = [0, ] * 6
-    for number in number_list:
-        results = [compute_polynomial(i, number) for i in range(1, 7)]
-        errors = [(r, abs(res - math.sin(number)))
+    for num in nums:
+        results = [compute_polynomial(i, num) for i in range(1, 7)]
+        errors = [(r, abs(res - math.sin(num)))
                   for r, res in enumerate(results, 1)]
 
         best_three = sorted(errors, key=lambda x: x[1])[:3]
-        best[number] = best_three
+        best[num] = best_three
         best_each[best_three[0][0] - 1] += 3
         best_each[best_three[1][0] - 1] += 2
         best_each[best_three[2][0] - 1] += 1
 
-    pprint.pprint(best)
-    pprint.pprint(best_each)
+    # pprint.pprint(best)
+    print('Cele mai bune polinoame: {}'.format(best_each))
+    print('')
 
-    number_list = [random.uniform(-math.pi / 2, math.pi / 2)
-                   for x in range(100000)]
+    nums = [random.uniform(-math.pi / 2, math.pi / 2) for x in range(100000)]
     for polynomial in range(1, 7):
         start_time = time.time()
-        for number in number_list:
-            compute_polynomial(polynomial, number)
-        print("polynomial {}: {}"
-              .format(str(polynomial), str(time.time() - start_time)))
+        [compute_polynomial(polynomial, num) for num in nums]
+        print("P{}: {:.5f} sec".format(polynomial, time.time() - start_time))
 
 
 if __name__ == '__main__':
